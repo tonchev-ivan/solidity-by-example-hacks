2a211b75d5d27009a2b1093d26621d48e2874dca
Ivan Tonchev <tonchev.b.ivan@gmail.com>
2022-11-16 23:37:40 +0200

    init repo

---
+++ contracts/ReEntrancyGuard.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.6;
+
+contract ReEntrancyGuard {
+    bool internal locked;
+
+    modifier noReentrant() {
+        require(!locked, "No re-entrancy");
+        locked = true;
+        _;
+        locked = false;
+    }
+}
